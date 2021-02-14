6c2d868c1d78f4f31f98dc52af0c6043617e001b
firat <firat@DESKTOP-CTB4JCJ>
2021-02-14 23:55:17 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
+import { ShoppingEditComponent } from './pages/shopping-list/shopping-edit/shopping-edit.component';
+import { ShoppingListComponent } from './pages/shopping-list/shopping-list.component';
+import { RecipeItemComponent } from './pages/recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './pages/recipes/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './pages/recipes/recipe-list/recipe-list.component';
+import { RecipesComponent } from './pages/recipes/recipes.component';
+import { HeaderComponent } from './layout/header/header.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
